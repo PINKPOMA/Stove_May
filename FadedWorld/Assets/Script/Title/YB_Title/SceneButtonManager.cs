81a853f7bc99a5165fd7003a679f53d4fb572e82
anminwoo <minwoo5995@gmail.com>
2022-05-21 15:51:12 +0900

    Add Explain Text

--- FadedWorld/Assets/Script/Title/YB_Title/SceneButtonManager.cs
+++ FadedWorld/Assets/Script/Title/YB_Title/SceneButtonManager.cs
@@ -2,9 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneButtonManager : MonoBehaviour
 {
+    private bool isActive;
+    
+    [SerializeField]private Text explainText;
+    [SerializeField] private GameObject HelpMessagePanel;
     public void StartButton()
     {
         SceneManager.LoadScene(1);
@@ -23,4 +28,29 @@ public class SceneButtonManager : MonoBehaviour
     {
         SceneManager.LoadScene(0);
     }
+    
+    
+    
+   public void OnClickHelpButton()
+   {
+       isActive = !isActive;
+        HelpMessagePanel.SetActive(isActive);
+    }
+
+   public void OnClickNextButton()
+   {
+       explainText.text = "생존 가능 시간: 회색에서 버틸 수 있는 시간입니다. (10초)\n" +
+                          "시계: 생존 가능 시간을 5초 추가해줍니다.\n" +
+                          "회색 지대, 회색 비: 닿아있으면 생존 가능 시간이 줄어듭니다.\n" +
+                          "가시: 밟으면 3초 동안 이동속도가 절반이 됩니다.\n" +
+                          "떨어지거나 생존 가능 시간이 0초가 된다면 게임 오버입니다.";
+   }
+
+   public void OnClickPerivousButton()
+   {
+       explainText.text = "이동 방법: a, d\n" +
+                          "점프: space (최대 2번)\n" +
+                          "걷기: shift키 누른 상태로 이동\n" +
+                          "Help 버튼을 다시 눌러 도움말을 종료할 수 있습니다.";
+   }
 }
